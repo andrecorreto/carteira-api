314e75b0e304f28bdeb16055f13126f6d5600f92
André Correto <andrecorreto@msn.com>
2021-11-28 17:29:05 -0300

    Ajuste no controller Home

--- src/main/java/br/com/alura/carteira/controller/HomeController.java
+++ src/main/java/br/com/alura/carteira/controller/HomeController.java
@@ -1,12 +1,19 @@
 package br.com.alura.carteira.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/home")
+@Api(tags = "Home")
 public class HomeController {
 
+	@GetMapping
+	@ApiOperation("Autenticar usuario")
 	public String home() {
 		return "Teste deploy automatico";
 	}
