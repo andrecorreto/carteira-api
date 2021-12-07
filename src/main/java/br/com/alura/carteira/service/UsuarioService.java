95a738d1a41d4f5a7fbfae8b765e6761f4d40765
André Correto <andrecorreto@msn.com>
2021-12-07 10:31:35 -0300

    Envio de email no cadastro de usuario

--- src/main/java/br/com/alura/carteira/service/UsuarioService.java
+++ src/main/java/br/com/alura/carteira/service/UsuarioService.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import br.com.alura.carteira.dto.UsuarioDto;
 import br.com.alura.carteira.dto.UsuarioFormDto;
+import br.com.alura.carteira.infra.EnviadorDeEmail;
 import br.com.alura.carteira.modelo.Perfil;
 import br.com.alura.carteira.modelo.Usuario;
 import br.com.alura.carteira.repository.PerfilRepository;
@@ -28,6 +29,8 @@ public class UsuarioService {
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	@Autowired
 	private PerfilRepository perfilRepository;
+	@Autowired
+	private EnviadorDeEmail enviadorDeEmail;
 	
 	public Page<UsuarioDto> listar(Pageable paginacao) {
 		Page<Usuario> usuarios = usuarioRepository.findAll(paginacao);
@@ -37,7 +40,7 @@ public class UsuarioService {
 	@Transactional
 	public UsuarioDto cadastrar(UsuarioFormDto dto) {
 		Usuario usuario = modelMapper.map(dto, Usuario.class);
-		
+		usuario.setId(null);		
 		Perfil perfil = perfilRepository.getById(dto.getPerfilId());
 		usuario.adicionarPerfil(perfil);
 		
@@ -46,6 +49,26 @@ public class UsuarioService {
 		usuario.setSenha(bCryptPasswordEncoder.encode(senha));
 		
 		usuarioRepository.save(usuario);
+		
+//		String mensagem = 
+//				"Você foi cadastrado no aplicativo carteira." +
+//				"seja bem vindo!" +
+//				" 	login: " + usuario.getLogin() +
+//				"   senha: " + senha;
+		String mensagem = String.format(
+				"Olá %s!\n\n"
+				+ "Seguem seus dados de acesso ao sistema Carteira:\n"
+				+ "Login: %s\n"
+				+ "Senha: %s", 
+				usuario.getNome(),
+				usuario.getLogin(),
+				senha);
+							
+		enviadorDeEmail.enviarEmail(
+				usuario.getEmail(), 
+				"Carteira - Bem vindo(a)", 
+				mensagem);
+		
 		return modelMapper.map(usuario,  UsuarioDto.class);
 	}
 }
